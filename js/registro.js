af1743c2f66211c730dca8426fc7d3758c20d138
16maax <maxcarrillo1616@gmail.com>
2023-05-03 10:55:23 -0600

    a

--- js/registro.js
+++ js/registro.js
@@ -7,8 +7,9 @@ btnGuardar.addEventListener("click", function(event) {
   const nombre = document.getElementById("nom").value;
   const curp = document.getElementById("curp").value;
   const telefono = document.getElementById("tel").value;
+  const correo = document.getElementById("email").value;
 
-  const usuario = { nombre, curp, telefono };
+  const usuario = { nombre, curp, telefono, correo };
   
   let usuarios = JSON.parse(localStorage.getItem("usuarios"));
   if (usuarios === null) {
@@ -22,4 +23,17 @@ btnGuardar.addEventListener("click", function(event) {
   document.getElementById("nom").value = "";
   document.getElementById("curp").value = "";
   document.getElementById("tel").value = "";
+  document.getElementById("email").value = "";
+});
+
+localStorage.removeItem('usuarios');
+
+const codigoBarras = document.getElementById('codigo-barras');
+JsBarcode(codigoBarras, "Denominación/Razón Social: REFACCIONAMIENTO Y COMERCIALIZACION PARA LA AUTOMATIZACION");
+JsBarcode(codigoBarras, "Denominación/Razón Social: REFACCIONAMIENTO Y COMERCIALIZACION PARA LA AUTOMATIZACION", {
+  width: 2,
+  height: 30,
+  format: "CODE128",
+  displayValue: true,
+  textPosition: "bottom"
 });
